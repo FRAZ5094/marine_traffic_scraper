1b6c882f07d7682ce2d86a9c4fc92e08d4fdea8a
FRAZ5094 <frasergmorrison@btinternet.com>
2021-10-08 22:00:03 +0100

    auto commit

--- latest_timestamp.bash
+++ latest_timestamp.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n 42 dundee_port_data.json | grep "timestamp" | awk '{print $2}' | sed s/,// | xargs -I {} echo "{}/1000" | bc | xargs -I {} date -d @{}
+grep "timestamp"  dundee_port_data.json | awk '{print $2}' | sed s/,// | xargs -I {} echo "{}/1000" | bc | xargs -I {} date -d @{}
