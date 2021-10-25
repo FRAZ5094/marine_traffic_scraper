67e7013bc5049e351859f03a477b8d709a739b4a
FRAZ5094 <frasergmorrison@btinternet.com>
2021-10-25 10:51:43 +0100

    data parsed correctly, need to save to csv next

--- parse_port_data.py
+++ parse_port_data.py
@@ -38,33 +38,54 @@ if __name__ == "__main__":
     # for key in boats.keys():
     # print(key)
 
-    date_end = datetime.fromtimestamp(time[0])
+    date_start = datetime.fromtimestamp(time[0])
+    print(date_start)
     date_end = datetime.fromtimestamp(time[-1])
+    print(date_end)
 
-    # print(boats["SEVEN PEGASUS"])
+    print(boats.keys())
 
-    for boat in boats.keys():
-        boats[boat]["in_port_times"] = [[]]
-        boats[boat]["out_port_times"] = [[]]
-        in_port_count = 0
-        out_port_count = 0
-        prev_in_port = boats[boat]["in_port"][0]
-        for i in range(1, len(boats[boat]["in_port"])):
-            if boats[boat]["in_port"][i] != prev_in_port:
-                if boats[boat]["in_port"][i]:
-                    boats[boat]["in_port_times"].append([])
-                    prev_in_port = not prev_in_port
-                else:
-                    boats[boat]["out_port_times"].append([])
-                    prev_in_port = not prev_in_port
-
+    # for boat in boats.keys():
+    boat = list(boats.keys())[10]
+    boats[boat]["in_port_times"] = []
+    boats[boat]["out_port_times"] = []
+    in_port_count = 0
+    out_port_count = 0
+    prev_in_port = None  # not true or false
+    for i in range(1, len(boats[boat]["in_port"])):
+        if boats[boat]["in_port"][i] != prev_in_port:
             if boats[boat]["in_port"][i]:
-                boats[boat]["in_port_times"][in_port_count].append(time[i])
+                boats[boat]["in_port_times"].append([])
+                print(f"appending to in_port_times at i= {i}")
+                print(time[i])
+                prev_in_port = not prev_in_port
             else:
-                boats[boat]["out_port_times"][out_port_count].append(time[i])
+                boats[boat]["out_port_times"].append([])
+                print(f"appending to out_port_times at i= {i}")
+                print(time[i])
+                prev_in_port = not prev_in_port
 
-    # for l in boats["SEVEN PEGASUS"]["in_port_times"]:
+        if boats[boat]["in_port"][i]:
+            boats[boat]["in_port_times"][-1].append(time[i])
+        else:
+            boats[boat]["out_port_times"][-1].append(time[i])
+
+    # for l in boats[boat]["in_port_times"]:
     # print(l)
 
-    print(len(boats["SEVEN PEGASUS"]["in_port_times"]))
-    print(len(boats["SEVEN PEGASUS"]["out_port_times"]))
+    print(len(boats[boat]["in_port_times"]))
+    print(len(boats[boat]["out_port_times"]))
+
+    print("----")
+    print(boat)
+
+    # calculate in/out of port delta_t's
+
+    for section in boats[boat]["in_port_times"]:
+        delta_t = (section[-1] - section[0]) / 60
+        if delta_t <= 10:
+            print(section)
+        print(delta_t)
+
+    plt.plot(time, boats[boat]["in_port"])
+    plt.show()
