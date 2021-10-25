40c3ade3be995fb11961220055fe1111a3c18f9e
FRAZ5094 <frasergmorrison@btinternet.com>
2021-10-25 16:40:34 +0100

    data outputted to csv

--- parse_port_data.py
+++ parse_port_data.py
@@ -38,54 +38,102 @@ if __name__ == "__main__":
     # for key in boats.keys():
     # print(key)
 
-    date_start = datetime.fromtimestamp(time[0])
+    date_start = datetime.fromtimestamp(time[0]).strftime("%d-%m-%y_%h:%m:%s")
     print(date_start)
-    date_end = datetime.fromtimestamp(time[-1])
+    date_end = datetime.fromtimestamp(time[-1]).strftime("%d-%m-%y_%H:%M:%S")
     print(date_end)
 
     print(boats.keys())
 
-    # for boat in boats.keys():
-    boat = list(boats.keys())[10]
-    boats[boat]["in_port_times"] = []
-    boats[boat]["out_port_times"] = []
-    in_port_count = 0
-    out_port_count = 0
-    prev_in_port = None  # not true or false
-    for i in range(1, len(boats[boat]["in_port"])):
-        if boats[boat]["in_port"][i] != prev_in_port:
+    for boat in boats.keys():
+        boats[boat]["in_port_times"] = []
+        boats[boat]["out_port_times"] = []
+        in_port_count = 0
+        out_port_count = 0
+        prev_in_port = None  # not true or false
+        for i in range(1, len(boats[boat]["in_port"])):
+            if boats[boat]["in_port"][i] != prev_in_port:
+                if boats[boat]["in_port"][i]:
+                    boats[boat]["in_port_times"].append([])
+                    prev_in_port = not prev_in_port
+                else:
+                    boats[boat]["out_port_times"].append([])
+                    prev_in_port = not prev_in_port
+
             if boats[boat]["in_port"][i]:
-                boats[boat]["in_port_times"].append([])
-                print(f"appending to in_port_times at i= {i}")
-                print(time[i])
-                prev_in_port = not prev_in_port
+                boats[boat]["in_port_times"][-1].append(time[i])
             else:
-                boats[boat]["out_port_times"].append([])
-                print(f"appending to out_port_times at i= {i}")
-                print(time[i])
-                prev_in_port = not prev_in_port
-
-        if boats[boat]["in_port"][i]:
-            boats[boat]["in_port_times"][-1].append(time[i])
-        else:
-            boats[boat]["out_port_times"][-1].append(time[i])
-
-    # for l in boats[boat]["in_port_times"]:
-    # print(l)
-
-    print(len(boats[boat]["in_port_times"]))
-    print(len(boats[boat]["out_port_times"]))
+                boats[boat]["out_port_times"][-1].append(time[i])
 
     print("----")
-    print(boat)
 
     # calculate in/out of port delta_t's
 
-    for section in boats[boat]["in_port_times"]:
-        delta_t = (section[-1] - section[0]) / 60
-        if delta_t <= 10:
-            print(section)
-        print(delta_t)
+    for boat in boats.keys():
+        in_port_delta_ts = []
+        out_port_delta_ts = []
 
-    plt.plot(time, boats[boat]["in_port"])
-    plt.show()
+        for section in boats[boat]["in_port_times"]:
+            delta_t = section[-1] - section[0]
+            in_port_delta_ts.append(int(delta_t / 60))  # in seconds
+
+        for section in boats[boat]["out_port_times"]:
+            delta_t = section[-1] - section[0]
+            out_port_delta_ts.append(int(delta_t / 60))  # in seconds
+
+        average_in_port = int(
+            ((sum(in_port_delta_ts)) / (len(in_port_delta_ts)) / 60)
+        )  # in minutes
+        boats[boat]["average_time_in_port"] = average_in_port
+        boats[boat]["number_in_port"] = len(boats[boat]["in_port_times"])
+        boats[boat]["number_out_port"] = len(boats[boat]["out_port_times"])
+
+        total_time_in_port = sum(in_port_delta_ts)
+        total_time_out_port = sum(out_port_delta_ts)
+        total_time = total_time_out_port + total_time_in_port
+
+        percent_in_port = int((total_time_in_port / total_time) * 100)
+
+        boats[boat]["percent_in_port"] = percent_in_port
+
+    boat_names = []
+    time_first_in_port = []
+    number_of_times_visited_port = []
+    number_of_times_out_of_port = []
+    average_time_spent_in_port_per_visit = []
+    percent_in_port = []
+    for boat in boats.keys():
+        boat_names.append(boat)
+
+        time_first = datetime.fromtimestamp(
+            boats[boat]["in_port_times"][0][0]
+        ).strftime("%d-%m-%y %H:%M:%S")
+
+        time_first_in_port.append(time_first)
+
+        number_of_times_visited_port.append(boats[boat]["number_in_port"])
+        number_of_times_out_of_port.append(boats[boat]["number_out_port"])
+        average_time_spent_in_port_per_visit.append(boats[boat]["average_time_in_port"])
+        percent_in_port.append(boats[boat]["percent_in_port"])
+
+    print(len(boat_names))
+    print(len(time_first_in_port))
+    print(len(number_of_times_visited_port))
+    print(len(number_of_times_out_of_port))
+    print(len(average_time_spent_in_port_per_visit))
+    print(len(percent_in_port))
+
+    boat_data = pd.DataFrame(
+        {
+            "boat names": boat_names,
+            "time first in port": time_first_in_port,
+            "number of times visited port": number_of_times_visited_port,
+            "number of times out of port": number_of_times_out_of_port,
+            "average time spend in port per visit": average_time_spent_in_port_per_visit,
+            "percentage of time in port": percent_in_port,
+        }
+    )
+
+    print(boat_data["time first in port"])
+
+    boat_data.to_csv("boat_data.csv", index=False)
